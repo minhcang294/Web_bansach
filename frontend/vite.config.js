749316da538f54af01e7f04013d18e7f122c6348
minhcang294 <mminh3229@gmail.com>
2026-07-16 14:19:33 +0700

    Fix: Cấu hình lại chuẩn xác package.json và package-lock.json cho Frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,10 +7,4 @@ export default defineConfig({
     port: 3000,
   },
 });
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
