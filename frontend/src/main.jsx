29da4d120c4f12701945cb5e087cdc6ada4969db
minhcang294 <mminh3229@gmail.com>
2026-07-16 14:41:54 +0700

    Fix: Sửa lỗi xung đột code và trùng lặp cấu trúc trong App.jsx và main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./styles/theme.css";
+import "./index.css"; // Nếu bạn có file css chung
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -12,15 +12,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </BrowserRouter>
   </React.StrictMode>
 );
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
->>>>>>> 1bc58c603ee8e39bb3939f9736736400ae741851
